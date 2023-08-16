6252175ccf8dd3be64f9364387fc31c8c9db7251
Romie Bett <bett@Betts-MacBook-Air.local>
2023-08-16 19:59:07 +0300

    user page addition

--- project/src/components/usrPgComponents/navigationmenu/usrProfile/usrProfile.tsx
+++ project/src/components/usrPgComponents/navigationmenu/usrProfile/usrProfile.tsx
@@ -5,7 +5,7 @@ const UsrProfileBox:React.FC = ()=>{
       return(
         <div className="profile-big-container">
             <div className="profile-pic-box">
-              
+              <img className='usr-profile-pic' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSl1EaRUdc2ZQNoe6mVspPBFaYqvnxbhZHx0Q&usqp=CAU" alt="" />
             </div>
             <p>cashier</p>
         </div>
