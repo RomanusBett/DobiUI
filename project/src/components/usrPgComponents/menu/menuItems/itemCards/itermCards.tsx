9dc40aa10220a5faba83b74c3ebce531c5fae764
Romie Bett <bett@Betts-MacBook-Air.local>
2023-08-29 23:39:14 +0300

    creates a reusable button

--- project/src/components/usrPgComponents/menu/menuItems/itemCards/itermCards.tsx
+++ project/src/components/usrPgComponents/menu/menuItems/itemCards/itermCards.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import './itemCards.css';
+import HomeCss from './itemCards.module.css'
+import AlterButton from "../../../alterButton/alterButton";
+import OrderDetailsCss from '../../../checkout/orderdetails/orderdetails.module.css'
 
 interface ItemCardsProps {
     items: string[]
@@ -7,29 +9,34 @@ interface ItemCardsProps {
 
 const ItemCards: React.FC<ItemCardsProps> = ({ items }) => {
     return (
-        <div className="big-card-container">
+        <div className={HomeCss.bigCardContainer}>
             {items.map((item) => (
                 <div key={item}>
-                    <div className="big-card-box">
-                        <div className="img-prc-container">
-                            <div className="card-content-container">
+                    <div className={HomeCss.bigCardBox}>
+                        <div className={HomeCss.imgPrcContainer}>
+                            <div className={HomeCss.cardContentContainer}>
                                 <div>
-                                    <img className="dispImgItem" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" alt="" />
+                                    <img className={HomeCss.dispImgItem} src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" alt="" />
                                 </div>
                                 <div>
-                                    <p>price</p>
+                                    <p>ksh103</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="desc-inc-container">
-                            <div className="card-content-container">
+                        <div className={HomeCss.descIncContainer}>
                                 <div>
                                     <li>{item}</li>
+                                    <div>
+                                        <p>12 remaining</p>
+                                    </div>
                                 </div>
                                 <div>
-                                    <p>increment field</p>
+                                    <div className={HomeCss.menuItemVarBox}>
+                                    <AlterButton children='-' className={`${OrderDetailsCss.decButton} ${OrderDetailsCss.minusBtn}`} />
+                                    <p>3</p>
+                                    <AlterButton children='+' className={`${OrderDetailsCss.incButton} ${OrderDetailsCss.plusBtn}`} />
+                                    </div>
                                 </div>
-                            </div>
                         </div>
                     </div>
                 </div>
